7639de9a0a06b0a4daf7893e82c8a5c8f08ff307
makserski <s1764038@ed.ac.uk>
2019-03-30 21:02:33 +0000

    Update Level_3_main.py

--- Level 3/Level_3_main.py
+++ Level 3/Level_3_main.py
@@ -60,8 +60,9 @@ def on_key_down():
     if keyboard.space:
         game_active = True
         SPEED=3
-        turtle.dead = False
-        reset_turtle()
+        if turtle.dead:
+            turtle.dead = False
+            reset_turtle()
     if keyboard.escape:
         game_active = False
         SPEED=0
@@ -70,9 +71,9 @@ def on_key_down():
 
 def update_turtle():
     if turtle.colliderect(trash1) or turtle.colliderect(trash2) or turtle.colliderect(trash3) or turtle.colliderect(trash4):
-        turtle.dead = True
+
         turtle.image = 'player1dead'
-        #clock.schedule(blackscreen, 1.0)
+        #clock.schedule_unique(blackscreen, 1.0)
         global game_active
         game_active = 0
         reset_turtle()
