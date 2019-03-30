c8ce3eae589107d04f3453e6284999a485d533f4
wafelkafel <49100185+wafelkafel@users.noreply.github.com>
2019-03-30 17:48:31 +0000

    Update Level_3_main.py

--- Level 3/Level_3_main.py
+++ Level 3/Level_3_main.py
@@ -2,20 +2,19 @@ import random
 
 WIDTH = 800
 HEIGHT = 600
-GAP = 130
 SPEED = 3
-GRAVITY = 0.3
-FLAP_VELOCITY = 40
+VELOCITY = 40
 
-turtle = Actor('ship', (75, 200))
+turtle = Actor('turtle', (75, HEIGHT//2))
 trash1 = Actor('sixpackrings', anchor=('center', 'center'))
+trash2 = Actor('straw', anchor=('center', 'center'))
 
 def draw():
     screen.blit('ocean1', (0, 0))
     #screen.draw.text("boi" , color='white', midtop=(WIDTH//2, 40), fontsize=70)
 
     trash1.draw()
-
+    trash2.draw()
     turtle.draw()
 
 # Initial state of the bird
@@ -23,9 +22,9 @@ turtle.dead = False
 
 def on_key_down():
     if keyboard.up:
-        turtle.y -= FLAP_VELOCITY
+        turtle.y -= VELOCITY
     if keyboard.down:
-        turtle.y += FLAP_VELOCITY
+        turtle.y += VELOCITY
 
 def update_turtle():
 
@@ -40,15 +39,19 @@ def update_turtle():
         turtle.vy = 0
         reset_trash()
 
-def reset_trash():
+def reset_trash1():
     trash1.pos = (WIDTH, random.randint(40,HEIGHT-40))
+
+def reset_trash2():
+    trash2.pos = (WIDTH, random.randint(40,HEIGHT-40))
 
 def update_trash():
     trash1.left -= SPEED
+    trash2.left -= SPEED
     if trash1.right < 0:
-        reset_trash()
-
-
+        reset_trash1()
+    if trash2.right < 0:
+        reset_trash2()
 
 
 reset_trash()
